eb12af9c22acd98b62930f940bced447ce52a1f4
Joris <Joris>
2016-04-09 12:01:03 +0300

    Basic Movement controller

---
+++ VR test/Assets/Scripts/MovementController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class MovementController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        Quaternion headRotation = InputTracking.GetLocalRotation(VRNode.Head);
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(moveDirection == Vector3.zero)
+        {
+            moveDirection = (headRotation * Vector3.forward);
+        }
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        if (Input.GetButton("Jump"))
+            moveDirection.y = jumpSpeed;
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
