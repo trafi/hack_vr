210694199bdc683e10f5f1f14af27dabd6c84f71
Joris <Joris>
2016-04-09 18:35:21 +0300

    Added PickupSpawner and sand

---
+++ VR test/Assets/PickupSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupSpawner : MonoBehaviour {
+
+    public GameObject pickup;
+    GameObject spawnedPickup;
+    Vector3 lastPos;
+
+    private Vector3 getPos()
+    {
+        return new Vector3(Random.Range(-100, 100), 5, Random.Range(-100, 100));
+    }
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Pickup spawner start");
+        lastPos = getPos();
+        spawnedPickup = (GameObject)Instantiate(pickup, lastPos, new Quaternion());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(spawnedPickup == null)
+        {
+            Vector3 newPos = getPos();
+            for (int i = 0; Vector3.Distance(newPos, lastPos) < 100; i++)
+            {
+                newPos = getPos();
+            }
+            Debug.Log("Distance " + Vector3.Distance(newPos, lastPos));
+            lastPos = newPos;
+            spawnedPickup = (GameObject)Instantiate(pickup, newPos, new Quaternion());
+        }
+        //Instantiate(pickup, getPos(), new Quaternion());
+    }
+}
