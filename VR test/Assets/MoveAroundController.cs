67e04fba4b420b209c14779d30f6ed70917be997
Nerijus Arlauskas <nercury@gmail.com>
2016-04-09 10:36:54 +0300

    Pawn Mock.

---
+++ VR test/Assets/MoveAroundController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveAroundController : MonoBehaviour {
+
+	private Vector3 direction;
+	private float maxDistance;
+
+	void Start () {
+		direction = (-transform.position).normalized;
+		direction.y = 0;
+		maxDistance = transform.position.magnitude;
+	}
+
+	void FixedUpdate () {
+		transform.position += direction;
+		if (transform.position.magnitude > maxDistance) {
+			direction = -direction;
+		}
+	}
+}
