8e67ad6b595669da65bee451bb1081a4b5cd4083
Justas Medeisis <justas@trafi.com>
2016-04-09 16:17:11 +0300

    Automatically connect to host or become one if existing host is not found within 5s.

--- VR test/Assets/Scripts/NetworkExperiments/AladdinNetworkManager.cs
+++ VR test/Assets/Scripts/NetworkExperiments/AladdinNetworkManager.cs
@@ -6,53 +6,124 @@ public class AladdinNetworkManager : NetworkManager {
 
 	public NetworkDiscovery discovery;
 
+	private enum State {
+		DISCONNECTED,
+		HOST,
+		CLIENT
+	}
+	private State state;
+
 	// Use this for initialization
 	void Start () {
-	
+		state = State.DISCONNECTED;
+
+		discovery.Initialize ();
+		StartCoroutine (EstablishConnection ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.touchCount > 0) {
-			Touch touch = Input.GetTouch (0);
-			if (touch.phase == TouchPhase.Began) {
-				float x = touch.position.x / Screen.width;
-				float y = touch.position.y / Screen.height;
-				if (x < 0.5) {
-					if (y < 0.5) {
-						Debug.Log ("Starting host");
-						StartHost ();
-					} else {
-						Debug.Log ("Starting client");
-						StartClient ();
-					}
-				} else {
-					if (y < 0.5) {
-						if (!discovery.isServer) {
-							Debug.Log ("Starting discovery as server");
-							discovery.Initialize ();
-							discovery.StartAsServer ();
-						}
-					} else {
-						if (!discovery.isClient) {
-							Debug.Log ("Starting discovery as client");
-							discovery.Initialize ();
-							discovery.StartAsClient ();
-						}
-					}
-				}
-			}
+
+	IEnumerator EstablishConnection() {
+		Debug.Log ("Establishing connection: Searching for host");
+		discovery.StartAsClient ();
+		yield return new WaitForSeconds (5);
+		if (State.DISCONNECTED != state) {
+			yield break;
 		}
+		Debug.Log ("Establishing connection: No host found, will become host");
+		discovery.StopBroadcast ();
+		StartHost ();
+		discovery.StartAsServer ();
+	}
+
+	// called by AladdinNetworkDiscovery
+	public void OnReceivedBroadcast(string fromAddress) {
+		Debug.Log ("Establishing connection: Host found " + fromAddress);
+		discovery.StopBroadcast ();
+		networkAddress = fromAddress;
+		StartClient();
 	}
 
 	public override void OnStartHost ()
 	{
-		base.OnStartHost ();
 		Debug.Log ("OnStartHost");
+		state = State.HOST;
 	}
 
-	public override void OnServerConnect(NetworkConnection conn)
+	public override void OnStartClient (NetworkClient client)
 	{
-		Debug.Log ("OnPlayerConnected");
+		if (State.HOST == state) {
+			return;
+		}
+		Debug.Log ("OnStartClient");
+		state = State.CLIENT;
 	}
+
+	/*
+	 * Customization available below.
+	 * See http://docs.unity3d.com/Manual/UNetManager.html
+	 * ---------------------------------------------------------------------------------------------------
+	 */
+	/*
+	 * HOST / SERVER methods
+	 */
+	/*
+	// called when a client connects 
+	public virtual void OnServerConnect(NetworkConnection conn);
+
+	// called when a client disconnects
+	public virtual void OnServerDisconnect(NetworkConnection conn)
+	{
+		NetworkServer.DestroyPlayersForConnection(conn);
+	}
+
+	// called when a client is ready
+	public virtual void OnServerReady(NetworkConnection conn)
+	{
+		NetworkServer.SetClientReady(conn);
+	}
+
+	// called when a new player is added for a client
+	public virtual void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
+	{
+		var player = (GameObject)GameObject.Instantiate(playerPrefab, playerSpawnPos, Quaternion.identity);
+		NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
+	}
+
+	// called when a player is removed for a client
+	public virtual void OnServerRemovePlayer(NetworkConnection conn, short playerControllerId)
+	{
+		PlayerController player;
+		if (conn.GetPlayer(playerControllerId, out player))
+		{
+			if (player.NetworkIdentity != null && player.NetworkIdentity.gameObject != null)
+				NetworkServer.Destroy(player.NetworkIdentity.gameObject);
+		}
+	}
+
+	// called when a network error occurs
+	public virtual void OnServerError(NetworkConnection conn, int errorCode);
+	*/
+
+	/*
+	 * CLIENT methods
+	 */
+	/*
+	// called when connected to a server
+	public virtual void OnClientConnect(NetworkConnection conn)
+	{
+		ClientScene.Ready(conn);
+		ClientScene.AddPlayer(0);
+	}
+
+	// called when disconnected from a server
+	public virtual void OnClientDisconnect(NetworkConnection conn)
+	{
+		StopClient();
+	}
+
+	// called when a network error occurs
+	public virtual void OnClientError(NetworkConnection conn, int errorCode);
+
+	// called when told to be not-ready by a server
+	public virtual void OnClientNotReady(NetworkConnection conn);
+	*/
 }
--- VR test/Assets/Scripts/NetworkExperiments/NetworkPlayerMove.cs
+++ VR test/Assets/Scripts/NetworkExperiments/NetworkPlayerMove.cs
@@ -2,21 +2,20 @@
 using UnityEngine.Networking;
 using System.Collections;
 
-public class PlayerMove : NetworkBehaviour
-{
-	public override void OnStartLocalPlayer()
-	{
+public class NetworkPlayerMove : NetworkBehaviour {
+	
+	public override void OnStartLocalPlayer() {
 		GetComponent<MeshRenderer>().material.color = Color.red;
 	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer) {
+			return;
+		}
+		var x = Input.GetAxis("Horizontal")*0.1f;
+		var z = Input.GetAxis("Vertical")*0.1f;
 
-    void Update()
-    {
-    	if(!isLocalPlayer) {
-    		return;
-    	}
-        var x = Input.GetAxis("Horizontal")*0.1f;
-        var z = Input.GetAxis("Vertical")*0.1f;
-
-        transform.Translate(x, 0, z);
-    }
+		transform.Translate(x, 0, z);
+	}
 }
